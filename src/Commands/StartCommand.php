25e11da5113afe39378620b1bfd4e914485238db
Hasin Hayder <countdraculla@gmail.com>
2021-02-15 00:31:51 +0600

    Added a new command Start & Code Cleanup

---
+++ src/Commands/StartCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class StartCommand extends Command
+{
+  protected function configure()
+  {
+    $this
+      ->setName('start')
+      ->setDescription('Start a WordPress SQLite Site')
+      ->addArgument("name", InputArgument::REQUIRED, "Site Name");
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $siteName = $input->getArgument("name");
+    if(file_exists("{$siteName}.json")){
+      if (PHP_OS == "WIN32" || PHP_OS == "Windows" || PHP_OS == "WINNT") {
+        exec("php -S {$siteName}:80 -t {$siteName}/");
+      } else {
+        exec("sudo php -S {$siteName}:80 -t {$siteName}/");
+      }
+    }
+    return 1;
+  }
+}
