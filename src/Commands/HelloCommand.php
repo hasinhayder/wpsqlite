ae54cb8f583171ec0ed2e9d8ee4aa641f339b4e2
Hasin Hayder <countdraculla@gmail.com>
2021-02-15 02:33:50 +0600

    Code Cleanup

--- src/Commands/HelloCommand.php
+++ src/Commands/HelloCommand.php
@@ -3,9 +3,7 @@
 namespace App\Console\Commands;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class HelloCommand extends Command
--- src/Extensions/WPSQLiteApplication.php
+++ src/Extensions/WPSQLiteApplication.php
@@ -13,12 +13,6 @@ class WPSQLiteApplication extends Application{
         return new InputDefinition([
             new InputArgument('command', InputArgument::REQUIRED, 'The command to execute'),
             new InputOption('--help', '-h', InputOption::VALUE_NONE, 'Display help for the given command'),
-            //new InputOption('--quiet', '-q', InputOption::VALUE_NONE, 'Do not output any message'),
-            //new InputOption('--verbose', '-v|vv|vvv', InputOption::VALUE_NONE, 'Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug'),
-            //new InputOption('--version', '-V', InputOption::VALUE_NONE, 'Display this application version'),
-            //new InputOption('--ansi', '', InputOption::VALUE_NONE, 'Force ANSI output'),
-            //new InputOption('--no-ansi', '', InputOption::VALUE_NONE, 'Disable ANSI output'),
-            //new InputOption('--no-interaction', '-n', InputOption::VALUE_NONE, 'Do not ask any interactive question'),
         ]);
     }
 }
